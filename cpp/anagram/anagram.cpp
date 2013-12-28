a55601f2dcbb6bb1d5da07ce25772e602586d179
fuwenlong <fuwenlong@126.com>
2013-12-28 17:54:18 +1300

    add directories and files

---
+++ cpp/anagram/anagram.cpp
@@ -0,0 +1,30 @@
+/*
+ * File:anagram.cpp
+ * Data: 28 - Dec - 2013
+ * Author: Fuwenlong@126.com
+ * 
+ * Details:
+ * Compare two strings to check whether they are anagram worlds. 
+ *
+ */ 
+
+
+int main( int argc, char** argv )
+{
+// 1. read parameters
+
+
+// 2. load two strings. 
+// 
+
+// 3. build a algorithm model 
+
+
+// 4. check whether two strings have same letters. 
+
+
+// 5. give results.   
+  
+  
+  return 0;
+}
---
+++ cpp/test/utestanagram.cpp
@@ -0,0 +1,29 @@
+/*
+ * File:utestanagram.cpp 
+ * Data: 28 - Dec - 2013
+ * Author: fuwenlong@126.com
+ * 
+ * Details:
+ *  Unit tests for Anagram. 
+ *
+ * Test classes
+ * (1)
+ * (2)
+ * (3)
+ */
+
+
+
+// include cppunitttest 
+
+
+int main( int argc, char** argv )
+{
+  
+  
+  // ToDo 
+  
+ 
+  return 0;
+}
+
